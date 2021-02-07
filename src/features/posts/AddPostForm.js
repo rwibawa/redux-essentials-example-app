d5c4c376970af51d39e72f1dee80b6a95471636a
Ryan Wibawa <rwibawa@ehe.health>
2021-02-06 23:05:27 -0800

    Add more features to the posts

--- src/features/posts/AddPostForm.js
+++ src/features/posts/AddPostForm.js
@@ -1,28 +1,42 @@
-import { nanoid } from '@reduxjs/toolkit'
 import React, { useState } from 'react'
-import { useDispatch } from 'react-redux'
+import { useDispatch, useSelector } from 'react-redux'
 import { postAdded } from './postsSlice'
 
 export const AddPostForm = () => {
   const [title, setTitle] = useState('')
   const [content, setContent] = useState('')
+  const [userId, setUserId] = useState('')
 
   const dispatch = useDispatch()
+  const users = useSelector(state => state.users)
 
   const onTitleChanged = e => setTitle(e.target.value)
   const onContentChanged = e => setContent(e.target.value)
+  const onAuthorChanged = e => setUserId(e.target.value)
 
   const onSavePostClicked = () => {
     if (title && content) {
       dispatch(
-        postAdded({
-          id: nanoid(),
+        postAdded(
           title,
-          content
-        })
+          content,
+          userId
+        )
       )
+
+      setTitle('')
+      setContent('')
     }
   }
+
+  const canSave = Boolean(title) && Boolean(content) && Boolean(userId)
+
+  const userOptions = users.map(user => (
+    <option key={user.id} value={user.id}>
+      {user.name}
+    </option>
+  ))
+
   return (
     <section>
       <h2>Add a New Post</h2>
@@ -31,17 +45,26 @@ export const AddPostForm = () => {
         <input
           type="text"
           id="postTitle"
+          name="postTitle"
+          placeholder="What's on your mind?"
           value={title}
           onChange={onTitleChanged}
         />
-        <label htmlFor="postContent">Content</label>
+        <label htmlFor="postAuthor">Author:</label>
+        <select id="postAuthor" value={userId} onChange={onAuthorChanged}>
+          <option value=""></option>
+          {userOptions}
+        </select>
+        <label htmlFor="postContent">Content:</label>
         <textarea
           id="postContent"
           name="postContent"
           value={content}
           onChange={onContentChanged} 
         />
-        <button type="button" onClick={onSavePostClicked}>Save Post</button>
+        <button type="button" onClick={onSavePostClicked} disabled={!canSave}>
+          Save Post
+        </button>
       </form>
     </section>
   )
--- src/features/posts/PostsList.js
+++ src/features/posts/PostsList.js
@@ -1,13 +1,29 @@
 import React from 'react'
 import { useSelector } from 'react-redux'
+import { Link } from 'react-router-dom'
+import { PostAuthor } from './PostAuthor'
+import { ReactionButtons } from './ReactionButtons'
+import { TimeAgo } from './timeAgo'
 
 export const PostsList = () => {
   const posts = useSelector((state) => state.posts)
 
-  const renderedPosts = posts.map(post => (
+  // Sort posts in reverse chronological order by datetime string
+  const orderedPosts = posts.slice().sort((a, b) => b.date.localeCompare(a.date))
+
+  const renderedPosts = orderedPosts.map(post => (
     <article className="post-excerpt" key={post.id}>
       <h3>{post.title}</h3>
+      <div>
+        <PostAuthor userId={post.user} />
+        <TimeAgo timestamp={post.date} />
+      </div>
       <p className="post-content">{post.content.substring(0, 100)}</p>
+
+      <ReactionButtons post={post} />
+      <Link to={`/posts/${post.id}`} className="button muted-button">
+        View Post
+      </Link>
     </article>
   ))
 
